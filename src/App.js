222d7cd425276d789cf2dc3651070b37e625c409
seunghyun.oh <seunghyun.oh@hancom.com>
2023-12-05 19:37:15 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useMemo } from 'react';
 
 function App() {
 
@@ -56,9 +56,28 @@ function App() {
     )
   }
 
+  // useMemo 사용하면 더이상 함수 호출이 아니라 값
+  const getDiaryAnalysis = useMemo(
+    () => {
+    console.log("일기 분석 시작")
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount/data.length)*100;
+    return {goodCount, badCount, goodRatio};
+  }, [data.length]
+  );
+
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
+
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}/>
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit}/>
     </div>
   );
