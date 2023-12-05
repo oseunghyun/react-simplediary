bcabcacc0606461bcd4fd3b88904212d0efd3795
seunghyun.oh <seunghyun.oh@hancom.com>
2023-12-05 11:19:28 +0900

    데이터 수정하기

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,35 @@
-const DiaryItem = ({author, content, created_date, emotion, id, onDelete}) => {
+import {useState, useRef} from "react";
+
+const DiaryItem = ({author, content, created_date, emotion, id, onRemove, onEdit}) => {
+
+    const handleRemove = () => {
+        if(window.confirm(`${id}번째 일기를 삭제하시겠습니까?`)){
+            onRemove(id);
+        }
+        
+        console.log(id);
+    }
+
+    const [isEdit, setIsEdit] = useState(false);
+    const toggleIsEdit = () => setIsEdit(!isEdit);
+    const [localContent, setLocalContent] = useState("");
+    const localContentInput = useRef();
+    const handleQuitEdit = () => {
+        setIsEdit(false);
+        setLocalContent(content);
+    }
+    const handleEdit = () => {
+        if(localContent.length < 5) {
+            localContentInput.current.focus();
+            return;
+        }
+        if(window.confirm(`${id}번째 일기를 수정하시겠습니까?`)) {
+        onEdit(id, localContent);
+        toggleIsEdit();
+        }
+    }
+  
+
     return <div className="DiaryItem">
         <div className="info" >
             <span>작성자 : {author} | 감정 : {emotion}</span>
@@ -7,13 +38,20 @@ const DiaryItem = ({author, content, created_date, emotion, id, onDelete}) => {
                 {new Date(created_date).toLocaleString()}
             </span>
         </div>
-        <div className="content">일기 : {content}</div>
-        <button onClick={() => {
-            if(window.confirm(`${id}번째 일기를 삭제하시겠습니까?`)){
-                onDelete(id);
-            }
-            console.log(id);
-        }}>삭제하기</button>
+        <div className="content">
+            {isEdit ? (
+                <><textarea ref={localContentInput} value={localContent} onChange={(e)=>setLocalContent(e.target.value)}
+            /></>) : <>{content}</>}
+        </div>
+            
+        {isEdit ? 
+        <>
+        <button onClick={handleQuitEdit}>수정 취소</button>
+        <button onClick={handleEdit}>수정 완료</button>
+        </> : <>
+        <button onClick={handleRemove}>삭제하기</button>
+        <button onClick={toggleIsEdit}>수정하기</button></>}
+        
     </div>
 };
 
--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,12 +1,12 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({diaryList, onDelete}) => {
+const DiaryList = ({diaryList, onRemove, onEdit}) => {
     return <div className="DiaryList">
         <h2>일기 리스트</h2>
         <h4>{diaryList.length}개의 일기가 있습니다.</h4>
         <div>
             {diaryList.map((it, idx) => (
-                <DiaryItem key={it.id} {...it} onDelete={onDelete}/>
+                <DiaryItem key={it.id} {...it} onRemove={onRemove} onEdit={onEdit}/>
             
             ))}
         </div>
